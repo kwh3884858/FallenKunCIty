cde76caaf9533174109e9d1e0da0cdc12940fd82
Kong Wei Hang <kwh3884858@gmail.com>
2021-08-28 18:02:59 +0800

    Add Config Root as Config Singleton.

---
+++ Assets/Scripts/Config/ConfigRoot.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using StarPlatinum.Base;
+
+namespace Config.GameRoot
+{
+	/// <summary>
+	/// Only for editor
+	/// </summary>
+	[CreateAssetMenu (fileName = "ConfigRoot", menuName = "Config/SpawnConfigRoot", order = 1)]
+	public class ConfigRoot : ConfigSingleton<ConfigRoot>
+	{
+		[Header ("Start Scene")]
+		public SceneLookupEnum StartScene;
+	}
+}
