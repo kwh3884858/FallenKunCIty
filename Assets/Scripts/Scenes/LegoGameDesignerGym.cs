05aaa2ac2e126a1c27eb00bd804e998cb75de09e
Kong Wei Hang <kwh3884858@gmail.com>
2021-08-28 18:06:23 +0800

    Add scene LegoGameDesignerGym script.

---
+++ Assets/Scripts/Scenes/LegoGameDesignerGym.cs
@@ -0,0 +1,20 @@
+﻿using Skylight;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegoGameDesignerGym : BaseScene
+{
+	public override void SceneInit()
+	{
+	}
+
+	public override void SceneShow()
+	{
+	}
+
+	public override void SceneClose()
+	{
+	}
+
+}
