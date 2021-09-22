deb4ff5c56889f6be0b4ea3e08985409c8bca2c6
Kong Weihang <kwh3884858@126.com>
2021-09-22 23:52:12 +0800

    [Task] In the progressing of create a story manager to manage actor. Move story related file to specific folder.

---
+++ Assets/Scripts/Story/ActionEvent.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+public class ActionEvent : MonoBehaviour
+{
+    private string m_id;
+    private bool m_isLoaded = false;
+    private int m_currentIndex = 0;
+    public string [] [] m_ArrayData;
+
+    ActionEvent ()
+    {
+        StartCoroutine (LoadCSV ("mainStory.csv", OnFileLoaded));
+    }
+
+    public void OnFileLoaded ()
+    {
+        m_isLoaded = true;
+    }
+
+    private EActor GetActorByName (string name)
+    {
+        switch (name) {
+        case "teacher":
+            return EActor.Teacher;
+        case "girl":
+            return EActor.Girl;
+        default:
+            Assert.IsTrue (false);
+            break;
+        }
+        return EActor.Null;
+    }
+
+    private ELocation GetLocationByName (string name)
+    {
+        switch (name) {
+        case "desk":
+            return ELocation.Desk;
+        case "chair":
+            return ELocation.Chair;
+        case "window":
+            return ELocation.Window;
+        default:
+            Assert.IsTrue (false);
+            break;
+        }
+        return ELocation.Null;
+    }
+
+    IEnumerator LoadCSV (string fileName, UnityAction CompleteAction)
+    {
+        string sPath = Application.streamingAssetsPath + "/" + fileName;
+        Debug.Log ("sPath:" + sPath);
+        WWW www = new WWW (sPath);
+        while (!www.isDone) {
+            yield return null;
+        }
+        Debug.Log ("content2:" + www.text);
+        File.WriteAllText (Application.persistentDataPath + "/" + fileName, www.text, Encoding.GetEncoding ("utf-8"));
+        LoadFile (Application.persistentDataPath, fileName);
+        CompleteAction ();
+    }
+
+    private void LoadFile (string path, string fileName)
+    {
+        m_ArrayData = new string [0] [];
+        string fillPath = path + "/" + fileName;
+
+        string [] lineArray;
+        try {
+            lineArray = File.ReadAllLines (fillPath, Encoding.GetEncoding ("utf-8"));
+            Debug.Log ("file finded!");
+        } catch {
+            Debug.Log ("file do not find!");
+            return;
+        }
+
+        m_ArrayData = new string [lineArray.Length] [];
+        for (int i = 0; i < lineArray.Length; i++) {
+            m_ArrayData [i] = lineArray [i].Split (',');
+        }
+    }
+
+    private string GetVaule (int row, int col)
+    {
+        return m_ArrayData [row] [col];
+    }
+
+    public bool IsFinished ()
+    {
+        return m_isLoaded && m_currentIndex == m_ArrayData.Length;
+    }
+
+    public void Update ()
+    {
+        if (!m_isLoaded) {
+            return;
+        }
+
+        foreach (var command in m_ArrayData) {
+            switch (command [0]) {
+            case "move": {
+                    Assert.IsTrue (command.Length == 3);
+                    string who = command [1];
+                    string where = command [2];
+
+                    EActor actor = GetActorByName (who);
+                    ELocation location = GetLocationByName (where);
+
+                    GameObject actorGO = StoryManager.Instance ().GetActorGameobjectByEActor (actor);
+                    GameObject locationGO = StoryManager.Instance ().GetLocationGameobjectByELocation (location);
+
+                    NPCController controller = actorGO.GetComponent<NPCController> ();
+                    Assert.IsNotNull (controller);
+					if (controller) {
+                        controller. MoveToActionPoint (locationGO);
+                    }
+                }
+
+                break;
+            case "interact": {
+                    Assert.IsTrue (command.Length == 3);
+
+                    string who = command [1];
+                    string what = command [2];
+                }
+
+                break;
+            case "talk": {
+                    Assert.IsTrue (command.Length > 3);
+                }
+
+                break;
+            case "talkKey": {
+                    Assert.IsTrue (command.Length > 3);
+                }
+
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Story/StoryManager.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Skylight;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+public enum EActor
+{
+    Null,
+    Teacher,
+    Girl,
+}
+
+public enum ELocation
+{
+    Null,
+    Desk,
+    Chair,
+    Window,
+}
+
+public class StoryManager : MonoSingleton<StoryManager>
+{
+    public class ActionEvent : MonoBehaviour
+    {
+
+    }
+
+    public class StoryTrack
+    {
+        public void Update()
+        {
+            foreach (ActionEvent item in m_actionEventList)
+            {
+                if (!item.IsFinished())
+                {
+                    item.Update();
+                }
+            }
+        }
+
+        public void AddActionEvent(ActionEvent actionEvent)
+        {
+            if (!m_actionEventList.Contains(actionEvent))
+            {
+                m_actionEventList.Enqueue(actionEvent);
+            }
+        }
+
+        public bool IsNeedUpdate()
+        {
+            bool needUpdate = false;
+            foreach (ActionEvent item in m_actionEventList)
+            {
+                if (!item.IsFinished())
+                {
+                    needUpdate = true;
+                }
+            }
+            return needUpdate;
+        }
+
+        Queue<ActionEvent> m_actionEventList;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Assert.IsTrue (m_teacher != null);
+        Assert.IsTrue (m_girl != null);
+        Assert.IsTrue (m_desk != null);
+        Assert.IsTrue (m_chair != null);
+        Assert.IsTrue (m_window != null);
+
+        m_actors.Add(EActor.Teacher, m_teacher);
+        m_actors.Add(EActor.Girl, m_girl);
+
+        m_location.Add(ELocation.Desk, m_desk);
+        m_location.Add(ELocation.Chair, m_chair);
+        m_location.Add(ELocation.Window, m_window);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_track2.IsNeedUpdate())
+        {
+            m_track2.Update();
+        }
+        else
+        {
+            m_track1.Update ();
+        }
+    }
+
+    public GameObject GetActorGameobjectByEActor(EActor actor)
+	{
+        return m_actors [actor];
+	}
+
+    public GameObject GetLocationGameobjectByELocation(ELocation location)
+	{
+        return m_location [location];
+	}
+
+    StoryTrack m_track1;
+    StoryTrack m_track2;
+
+    private Dictionary<EActor, GameObject> m_actors;
+    private Dictionary<ELocation, GameObject> m_location;
+
+    [Header ("Actor")]
+    public GameObject m_teacher;
+    public GameObject m_girl;
+    [Header ("Location")]
+    public GameObject m_desk;
+    public GameObject m_chair;
+    public GameObject m_window;
+}
