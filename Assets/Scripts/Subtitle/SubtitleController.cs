df45021a5575574b5c7215cb09197eb7b39acfea
Kong Weihang <kwh3884858@126.com>
2021-09-24 22:26:35 +0800

    [Task] Update subtitle prefab. Add subtitle controller to destroy game object when the subtitle is finish.

---
+++ Assets/Scripts/Subtitle/SubtitleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (m_isStartCountDown) {
+            m_countDown -= Time.deltaTime;
+			if (m_countDown < 0) {
+                m_isStartCountDown = false;
+                Destroy (this);
+			}
+		}
+    }
+
+    public void StartSubtitile(string content)
+	{
+        m_countDown = m_delayPerWord * content.Length;
+        GetComponent<Text> ().text = content;
+        m_isStartCountDown = true;
+    }
+    public float m_delayPerWord = 0.5f;
+
+    private float m_countDown = 100.0f;
+    private bool m_isStartCountDown = false;
+}
--- Assets/Scripts/UI/Panel/UISubtitilePanel.cs
+++ Assets/Scripts/UI/Panel/UISubtitilePanel.cs
@@ -1,18 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Skylight;
 using UnityEngine;
 
-public class UISubtitilePanel : MonoBehaviour
+public class UISubtitilePanel : UIPanel
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	public override void PanelInit ()
+	{
+		base.PanelInit ();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		m_originalSubtitile = transform.Find ("Content/Subtitile").gameObject;
+		StarPlatinum.EventManager.EventManager.Instance.AddEventListener<TalkEvent> (OnTalkEvent);
+	}
+
+	private void OnTalkEvent (object sender, TalkEvent e)
+	{
+		AddSubtitile (e.m_content);
+	}
+
+	public override void PanelOpen ()
+	{
+		base.PanelOpen ();
+	}
+
+	public override void PanelClose ()
+	{
+		base.PanelClose ();
+	}
+
+	public void AddSubtitile (string content)
+	{
+		GameObject subtitile = Instantiate (m_originalSubtitile);
+		subtitile.name = content;
+		subtitile.transform.SetParent (this.transform);
+		SubtitleController controller = subtitile.GetComponent<SubtitleController> ();
+		controller.StartSubtitile (content);
+	}
+
+	GameObject m_originalSubtitile;
+
 }
