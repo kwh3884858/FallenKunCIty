9ef1b84ae1ae41824783b788039678eba6b341a5
Kong Wei Hang <kwh3884858@gmail.com>
2022-01-08 15:29:49 +0800

    [Task] - Support cut scene after click game start button.

--- Assets/Scripts/MovieStart.cs
+++ Assets/Scripts/MovieStart.cs
@@ -3,59 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+
 public class MovieStart : MonoBehaviour
 {
     private void Start()
     {
+        videoPlayer = GetComponent<VideoPlayer>();
+        //Debug.Log ("player");
+
+        // Play on awake defaults to true. Set it to false to avoid the url set
+        // below to auto-start playback since we're in Start().
+        videoPlayer.playOnAwake = false;
+
+        // By default, VideoPlayers added to a camera will use the far plane.
+        // Let's target the near plane instead.
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+
+        // Skip the first 100 frames.
+        videoPlayer.frame = 0;
+
+        //videoPlayer.targetCamera = Skylight.CameraService.Instance ().m_mainCamera.GetComponent<Camera> ();
+
+        // Restart from beginning when done.
+        videoPlayer.isLooping = true;
+
         if (m_playOnStart)
         {
-			Play();
+            Play();
 		}
     }
 
     // Use this for initialization
-    void Play ()
+    public void Play (VideoPlayer.EventHandler callback = null)
 	{
 
-		videoPlayer = GetComponent<VideoPlayer> ();
-		//Debug.Log ("player");
+        if (callback != null)
+        {
+            videoPlayer.loopPointReached += callback;
+        }
+        videoPlayer.loopPointReached += EndReached;
+        videoPlayer.Play ();
 
-		// Play on awake defaults to true. Set it to false to avoid the url set
-		// below to auto-start playback since we're in Start().
-		videoPlayer.playOnAwake = false;
+        if (m_backMask) m_backMask.SetActive(true);
+    }
 
-		// By default, VideoPlayers added to a camera will use the far plane.
-		// Let's target the near plane instead.
-		videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
-
-		// Skip the first 100 frames.
-		videoPlayer.frame = 0;
-
-		//videoPlayer.targetCamera = Skylight.CameraService.Instance ().m_mainCamera.GetComponent<Camera> ();
-
-		// Restart from beginning when done.
-		videoPlayer.isLooping = true;
-
-		// Each time we reach the end, we slow down the playback by a factor of 10.
-		videoPlayer.loopPointReached += EndReached;
-
-		videoPlayer.Play ();
-	}
-
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update ()
 	{
-		if (videoPlayer.frame == 0) {
-			videoPlayer.Play ();
-		}
-		if (Input.GetMouseButton (0) || Input.GetButtonDown ("Jump")) {
-			EndReached ();
+        if (!videoPlayer || !videoPlayer.isPlaying)
+        {
+            return;
+        }
+        if (Input.GetMouseButton (0) || Input.GetButtonDown ("Jump")) {
+            videoPlayer.frame = (long)videoPlayer.frameCount -1;
 		}
 	}
 
-	void EndReached (UnityEngine.Video.VideoPlayer vp = null)
+	void EndReached (UnityEngine.Video.VideoPlayer vp)
 	{
-		GameObject.Find ("GhostCatMainMenu/BlackMask").SetActive (false);
+        vp.loopPointReached -= EndReached;
+        if(m_backMask) m_backMask.SetActive (false);
 		SoundService.Instance().PlayMusic(m_bgmName, true);
 
 		gameObject.SetActive (false);
@@ -63,5 +70,6 @@ public class MovieStart : MonoBehaviour
 
 	public string m_bgmName;
 	public bool m_playOnStart;
+    public GameObject m_backMask;
     VideoPlayer videoPlayer;
 }
--- Assets/Scripts/UI/Panel/GhostCatMainMenu.cs
+++ Assets/Scripts/UI/Panel/GhostCatMainMenu.cs
@@ -6,67 +6,73 @@ using Config.GameRoot;
 
 public class GhostCatMainMenu : UIPanel
 {
-	public override void PanelInit ()
+	public override void PanelInit()
 	{
-		base.PanelInit ();
-		AddButtonClick ("GameStart", GameStart);
-		AddButtonClick ("About", GameAbout);
-		AddButtonClick ("Quit", GameQuit);
-		LogicManager.Instance ().Notify ((int)SkylightStaticData.LogicType.MainMenuInit);
-		EventTriggerListener.Get (transform.Find ("GameStart")).OnPreHover (1.2f);
-		EventTriggerListener.Get (transform.Find ("Setting")).OnPreHover (1.2f);
-		EventTriggerListener.Get (transform.Find ("About")).OnPreHover (1.2f);
-		EventTriggerListener.Get (transform.Find ("Quit")).OnPreHover (1.2f);
+		base.PanelInit();
+		AddButtonClick("GameStart", GameStart);
+		AddButtonClick("About", GameAbout);
+		AddButtonClick("Quit", GameQuit);
+		LogicManager.Instance().Notify((int)SkylightStaticData.LogicType.MainMenuInit);
+		EventTriggerListener.Get(transform.Find("GameStart")).OnPreHover(1.2f);
+		EventTriggerListener.Get(transform.Find("Setting")).OnPreHover(1.2f);
+		EventTriggerListener.Get(transform.Find("About")).OnPreHover(1.2f);
+		EventTriggerListener.Get(transform.Find("Quit")).OnPreHover(1.2f);
 	}
 
-	public override void PanelOpen ()
+	public override void PanelOpen()
 	{
-		base.PanelOpen ();
-		LogicManager.Instance ().Notify ((int)SkylightStaticData.LogicType.MainMenuOpen);
+		base.PanelOpen();
+		LogicManager.Instance().Notify((int)SkylightStaticData.LogicType.MainMenuOpen);
 	}
 
-	public override void PanelClose ()
+	public override void PanelClose()
 	{
-		base.PanelClose ();
-		LogicManager.Instance ().Notify ((int)SkylightStaticData.LogicType.MainMenuClose);
+		base.PanelClose();
+		LogicManager.Instance().Notify((int)SkylightStaticData.LogicType.MainMenuClose);
 	}
 
 
-	void GameStart ()
+	void GameStart()
 	{
-		UIManager.Instance ().ClosePanel<GhostCatMainMenu> ();
-		SoundService.Instance ().StopMusic ();
+		SoundService.Instance().StopMusic();
+		GameObject.Find("GhostCatMainMenu/GameStartMovie").GetComponent<MovieStart>().Play(EndReached);
+	}
 
-		SoundService.Instance ().PlayMusic ("GameBgm", true);
-		SoundService.Instance ().PlayEffect ("WaterDrop", true, 0.3f);
-		SceneLookupEnum startScene = ConfigRoot.Instance.StartScene;
+	void GameAbout()
+	{
+		UIManager.Instance().ShowPanel<UIGameInfoPanel>();
+	}
+
+	void GameQuit()
+	{
+		Application.Quit();
+
+	}
+
+	void EndReached(UnityEngine.Video.VideoPlayer vp)
+	{
+        vp.loopPointReached -= EndReached;
+
+        UIManager.Instance().ClosePanel<GhostCatMainMenu>();
+
+        //SoundService.Instance ().PlayMusic ("GameBgm", true);
+        //SoundService.Instance ().PlayEffect ("WaterDrop", true, 0.3f);
+        SceneLookupEnum startScene = ConfigRoot.Instance.StartScene;
         switch (startScene)
         {
             case SceneLookupEnum.GameRoot:
                 break;
-			case SceneLookupEnum.GhostCatMain:
-				SceneManager.Instance().ShowScene<GhostCatMain>();
-				break;
+            case SceneLookupEnum.GhostCatMain:
+                SceneManager.Instance().ShowScene<GhostCatMain>();
+                break;
             case SceneLookupEnum.LegoGameDesignerGym:
-				SceneManager.Instance().ShowScene<LegoGameDesignerGym>();
-				break;
+                SceneManager.Instance().ShowScene<LegoGameDesignerGym>();
+                break;
             case SceneLookupEnum.SceneCave:
-				SceneManager.Instance().ShowScene<SceneCave>();
-				break;
+                SceneManager.Instance().ShowScene<SceneCave>();
+                break;
             default:
                 break;
         }
-
-	}
-
-	void GameAbout ()
-	{
-		UIManager.Instance ().ShowPanel<UIGameInfoPanel> ();
-	}
-
-	void GameQuit ()
-	{
-		Application.Quit ();
-
 	}
 }
